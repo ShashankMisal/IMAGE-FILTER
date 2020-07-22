baf3783687409a2b63a6cc4ec00eb8b9c409967f
Shashank Misal <shaank1818@gmail.com>
2020-07-22 13:01:01 +0530

    Made Fuctions,variables and id's Self Descriptive. Applied a "check" condition to all functions.

--- main/function.js
+++ main/function.js
@@ -21,60 +21,72 @@ function upload2() {
 function changeSize() {
   alert("\t\tRestoring Sizes \nEverything under control");
   BACKGROUND_IMG.setSize(FOREGROUND_IMG.getWidth(), FOREGROUND_IMG.getHeight());
-  var bgcontext = SECOND_CANVAS_ID.getContext("2d");
-  bgcontext.clearRect(0, 0, FIRST_CANVAS_ID.width, FIRST_CANVAS_ID.height);
+  var secondCanvasContext = SECOND_CANVAS_ID.getContext("2d");
+  secondCanvasContext.clearRect(0, 0, FIRST_CANVAS_ID.width, FIRST_CANVAS_ID.height);
   BACKGROUND_IMG.drawTo(SECOND_CANVAS_ID);
 }
 
-function mergeGreenScreen() {
 
+function isForeGroundImageUploaded() {
   if (FOREGROUND_IMG == null || !FOREGROUND_IMG.complete()) {
     alert("Please upload a Foreground image");
+    return false;
   }
+  return true;
+}
+
+function isBackGroundImageUploaded() {
   if (BACKGROUND_IMG == null || !BACKGROUND_IMG.complete()) {
     alert("Please upload a Background image");
+    return false;
   }
+  return true;
+}
+
+
+
+function checkImageSize() {
   var wbg = BACKGROUND_IMG.getWidth();
   var wfg = FOREGROUND_IMG.getWidth();
   var hbg = BACKGROUND_IMG.getHeight();
   var hfg = FOREGROUND_IMG.getHeight();
+
   if (wbg != wfg || hbg != hfg) {
     alert("sizes are not the same");
     changeSize();
   }
-  var c;
-  COMPOSITE_IMG = new SimpleImage(FOREGROUND_IMG.getWidth(), FOREGROUND_IMG.getHeight());
-  for (var pix of FOREGROUND_IMG.values()) {
-    var x = pix.getX();
-    var y = pix.getY();
+}
 
 
 
-    if ((pix.getGreen() > 240) || (pix.getGreen() > (pix.getBlue() + pix.getRed()))) {
-      c = BACKGROUND_IMG.getPixel(x, y);
-      COMPOSITE_IMG.setPixel(x, y, c);
+function mergeGreenScreen() {
+
+  if (isForeGroundImageUploaded() && isBackGroundImageUploaded()) {
+    checkImageSize();
+    var c;
+    COMPOSITE_IMG = new SimpleImage(FOREGROUND_IMG.getWidth(), FOREGROUND_IMG.getHeight());
+    for (var pix of FOREGROUND_IMG.values()) {
+      var x = pix.getX();
+      var y = pix.getY();
+
+      if ((pix.getGreen() > 240) || (pix.getGreen() > (pix.getBlue() + pix.getRed()))) {
+        c = BACKGROUND_IMG.getPixel(x, y);
+        COMPOSITE_IMG.setPixel(x, y, c);
+      }
+      else {
+        COMPOSITE_IMG.setPixel(x, y, pix);
+      }
+
     }
-    else {
-      COMPOSITE_IMG.setPixel(x, y, pix);
-    }
-
+    COMPOSITE_IMG.drawTo(COMPOSITE_CANVAS_ID);
   }
-  COMPOSITE_IMG.drawTo(COMPOSITE_CANVAS_ID);
 }
 
 function clearBits(value) {
-  var x = Math.floor(value / 16) * 16;
-  return x;
+  return Math.floor(value / 16) * 16;
 }
 
-function extractBits(value) {
-  return (value % 16) * 16;
-
-
-}
-
-
-function chop2hide(image) {
+function chopImage1(image) {
   for (var px of image.values()) {
     px.setRed(clearBits(px.getRed()));
     px.setGreen(clearBits(px.getGreen()));
@@ -83,8 +95,7 @@ function chop2hide(image) {
   return image;
 }
 
-
-function shift(image) {
+function chopImage2(image) {
   for (var px of image.values()) {
     px.setRed(px.getRed() / 16);
     px.setGreen(px.getGreen() / 16);
@@ -95,7 +106,7 @@ function shift(image) {
 }
 
 
-function combine(start, hide) {
+function combineImages(start, hide) {
   var answer = new SimpleImage(start.getWidth(), start.getHeight());
 
   for (var px of answer.values()) {
@@ -115,29 +126,23 @@ function combine(start, hide) {
 
 function encrypt() {
 
-  if (FOREGROUND_IMG == null || !FOREGROUND_IMG.complete()) {
-    alert("Please upload a Foreground image");
-  }
-  if (BACKGROUND_IMG == null || !BACKGROUND_IMG.complete()) {
-    alert("Please upload a Background image");
-  }
-  var wbg = BACKGROUND_IMG.getWidth();
-  var wfg = FOREGROUND_IMG.getWidth();
-  var hbg = BACKGROUND_IMG.getHeight();
-  var hfg = FOREGROUND_IMG.getHeight();
-  if (wbg != wfg || hbg != hfg) {
-    alert("sizes are not the same");
-    changeSize();
-  }
+  if (isForeGroundImageUploaded() && isBackGroundImageUploaded()) {
+    checkImageSize();
 
-  FOREGROUND_IMG = chop2hide(FOREGROUND_IMG);
-  BACKGROUND_IMG = shift(BACKGROUND_IMG);
-  ENCRYPTED_IMG = combine(FOREGROUND_IMG, BACKGROUND_IMG);
+    FOREGROUND_IMG = chopImage1(FOREGROUND_IMG);
+    BACKGROUND_IMG = chopImage2(BACKGROUND_IMG);
+    ENCRYPTED_IMG = combineImages(FOREGROUND_IMG, BACKGROUND_IMG);
 
-  ENCRYPTED_IMG.drawTo(ENCRYPT_CANVAS_ID);
+    ENCRYPTED_IMG.drawTo(ENCRYPT_CANVAS_ID);
+  }
 }
 
-function extract(image) {
+
+function extractBits(value) {
+  return (value % 16) * 16;
+}
+
+function extractHiddenImage(image) {
   for (var px of image.values()) {
     px.setRed(extractBits(px.getRed()));
     px.setGreen(extractBits(px.getGreen()));
@@ -146,76 +151,87 @@ function extract(image) {
   return image;
 }
 
-
 function decrypt() {
-  var extractedImg = extract(ENCRYPTED_IMG);
-  // var dec = document.getElementById("dec");
-  extractedImg.drawTo(DECRYPT_CANVAS_ID);
+  if (isForeGroundImageUploaded() && isBackGroundImageUploaded()) {
+    if (ENCRYPTED_IMG != null) {
+      var extractedImg = extractHiddenImage(ENCRYPTED_IMG);
+      extractedImg.drawTo(DECRYPT_CANVAS_ID);
+    }
+    else {
+      alert("Please Crypt the Image before Decrypting");
+    }
+
+  }
 }
 
 function makegray() {
-  GRAY_IMG = new SimpleImage(FOREGROUND_IMG.getWidth(), FOREGROUND_IMG.getHeight());
-  for (var pix of FOREGROUND_IMG.values()) {
-    var x = pix.getX();
-    var y = pix.getY();
-    var g = GRAY_IMG.getPixel(x, y);
-    var grayc = ((pix.getGreen() + pix.getRed() + pix.getBlue()) / 3);
-    g.setGreen(grayc);
-    g.setRed(grayc);
-    g.setBlue(grayc);
+  if (isForeGroundImageUploaded()) {
+    GRAY_IMG = new SimpleImage(FOREGROUND_IMG.getWidth(), FOREGROUND_IMG.getHeight());
+    for (var pix of FOREGROUND_IMG.values()) {
+      var x = pix.getX();
+      var y = pix.getY();
+      var g = GRAY_IMG.getPixel(x, y);
+      var grayc = ((pix.getGreen() + pix.getRed() + pix.getBlue()) / 3);
+      g.setGreen(grayc);
+      g.setRed(grayc);
+      g.setBlue(grayc);
+    }
+    GRAY_IMG.drawTo(GRAY_CANVAS_ID);
   }
-  GRAY_IMG.drawTo(GRAY_CANVAS_ID);
 }
 function doOverlay() {
-  OVERLAY_IMG = FOREGROUND_IMG;
-  for (var pix of FOREGROUND_IMG.values()) {
-    var x = pix.getX();
-    var y = pix.getY();
-    var m = OVERLAY_IMG.getPixel(x, y);
-    if (pix.getX() <= FOREGROUND_IMG.getWidth() / 3) {
-      m.setRed(255);
-      m.setAlpha(100);
+  if (isForeGroundImageUploaded()) {
+    OVERLAY_IMG = FOREGROUND_IMG;
+    for (var pix of FOREGROUND_IMG.values()) {
+      var x = pix.getX();
+      var y = pix.getY();
+      var m = OVERLAY_IMG.getPixel(x, y);
+      if (pix.getX() <= FOREGROUND_IMG.getWidth() / 3) {
+        m.setRed(255);
+        m.setAlpha(100);
+      }
+      if ((pix.getX() > FOREGROUND_IMG.getWidth() / 3) && pix.getX() <= (FOREGROUND_IMG.getWidth() / 3) * 2) {
+        m.setBlue(255);
+        m.setAlpha(100);
+      }
+      if ((pix.getX() > ((FOREGROUND_IMG.getWidth() / 3) * 2)) && (pix.getX() <= FOREGROUND_IMG.getWidth())) {
+        m.setGreen(255);
+        m.setAlpha(100);
+      }
     }
-    if ((pix.getX() > FOREGROUND_IMG.getWidth() / 3) && pix.getX() <= (FOREGROUND_IMG.getWidth() / 3) * 2) {
-      m.setBlue(255);
-      m.setAlpha(100);
-    }
-    if ((pix.getX() > ((FOREGROUND_IMG.getWidth() / 3) * 2)) && (pix.getX() <= FOREGROUND_IMG.getWidth())) {
-      m.setGreen(255);
-      m.setAlpha(100);
-    }
+    OVERLAY_IMG.drawTo(OVERLAY_CANVAS_ID);
   }
-  OVERLAY_IMG.drawTo(OVERLAY_CANVAS_ID);
 }
 
 function makeRainBow() {
-  RAINBOW_IMG = FOREGROUND_IMG;
-  var rectHeight = RAINBOW_IMG.getHeight();
-  var rectSegment = parseInt(rectHeight) / 7;
-  var Y;
-  var X;
-  for (pixel of RAINBOW_IMG.values()) {
-    X = pixel.getX();
-    Y = pixel.getY();
-    //      RAINBOW_IMG.setPixel(X, Y, pixel);
-    AVG_COLOR = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
-    if (Y >= 6 * parseInt(rectSegment)) {
-      doRed();
-    } else if (Y >= (5 * parseInt(rectSegment))) {
-      doOrange();
-    } else if (Y >= (4 * parseInt(rectSegment))) {
-      doYellow();
-    } else if (Y >= (3 * parseInt(rectSegment))) {
-      doGreen();
-    } else if (Y >= (2 * parseInt(rectSegment))) {
-      doBlue();
-    } else if (Y >= parseInt(rectSegment)) {
-      doIndigo();
-    } else {
-      doViolet();
+  if (isForeGroundImageUploaded()) {
+    RAINBOW_IMG = FOREGROUND_IMG;
+    var rectHeight = RAINBOW_IMG.getHeight();
+    var rectSegment = parseInt(rectHeight) / 7;
+    var Y;
+    var X;
+    for (pixel of RAINBOW_IMG.values()) {
+      X = pixel.getX();
+      Y = pixel.getY();
+      AVG_COLOR = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+      if (Y >= 6 * parseInt(rectSegment)) {
+        doRed();
+      } else if (Y >= (5 * parseInt(rectSegment))) {
+        doOrange();
+      } else if (Y >= (4 * parseInt(rectSegment))) {
+        doYellow();
+      } else if (Y >= (3 * parseInt(rectSegment))) {
+        doGreen();
+      } else if (Y >= (2 * parseInt(rectSegment))) {
+        doBlue();
+      } else if (Y >= parseInt(rectSegment)) {
+        doIndigo();
+      } else {
+        doViolet();
+      }
     }
+    RAINBOW_IMG.drawTo(RAINBOW_CANVAS_ID);
   }
-  RAINBOW_IMG.drawTo(RAINBOW_CANVAS_ID);
 }
 
 function doViolet() {
